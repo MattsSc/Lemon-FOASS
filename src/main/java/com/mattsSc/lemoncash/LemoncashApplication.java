f8f5f81d9f2f0e9a60a5143c9b62245da9349ae8
matias Scan <matias.scan@gmail.com>
2022-02-07 20:40:05 -0300

    add base project structure and update properties

---
+++ src/main/java/com/mattsSc/lemoncash/LemoncashApplication.java
@@ -0,0 +1,46 @@
+package com.mattsSc.lemoncash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@SpringBootApplication
+@RestController
+@ComponentScan(basePackages = {"com.mattsSc.lemoncash"})
+public class LemoncashApplication {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Docker World";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(LemoncashApplication.class, args);
+	}
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.mattsSc.lemoncash.controller"))
+				.paths(PathSelectors.any()).build().apiInfo(apiInfo());
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("API Lemoncash FOAAS")
+				.description("")
+				.build();
+	}
+
+}
---
+++ src/main/java/com/mattsSc/lemoncash/configuration/AppConfig.java
@@ -0,0 +1,46 @@
+package com.mattsSc.lemoncash.configuration;
+
+import com.mattsSc.lemoncash.configuration.interceptor.AuthorizationInterceptor;
+import com.mattsSc.lemoncash.utils.redis.RedisOperation;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.RestTemplate;
+
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        builder.defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
+        return builder.build();
+    }
+
+    @Bean
+    LettuceConnectionFactory lettuceConnectionFactory(@Value("${spring.redis.host}") final String host, @Value("${spring.redis.port}") final Integer port){
+        return new LettuceConnectionFactory(host, port);
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(LettuceConnectionFactory lettuceConnectionFactory) {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setKeySerializer(RedisSerializer.string());
+        template.setValueSerializer(RedisSerializer.string());
+        template.setConnectionFactory(lettuceConnectionFactory);
+        template.afterPropertiesSet();
+        return template;
+    }
+
+    @Bean
+    public AuthorizationInterceptor authorizationInterceptor(RedisOperation redisOperation){
+        return new AuthorizationInterceptor(redisOperation);
+    }
+
+}
