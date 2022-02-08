aad67872526ecfe1714fa8f66076ad8c92d42819
matias Scan <matias.scan@gmail.com>
2022-02-08 12:38:30 -0300

    optimize imports

--- src/main/java/com/mattsSc/lemoncash/LemoncashApplication.java
+++ src/main/java/com/mattsSc/lemoncash/LemoncashApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
